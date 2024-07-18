ff7a36f5d778925cbfcdf80d5dec4205d86827db
Torben Jost <torbenjost@proton.me>
2024-07-18 12:39:51 +0200

    change pizza display sizes

--- js-functions/pizza-analyzer/js/index.js
+++ js-functions/pizza-analyzer/js/index.js
@@ -12,12 +12,14 @@ pizzaInput1.addEventListener("input", () => {
   const pizzaSize1 = pizzaInput1.value;
   const pizzaSize2 = pizzaInput2.value;
   calculatePizzaGain(pizzaSize1, pizzaSize2);
+  updatePizzaDisplay(pizza1, pizzaSize1);
 });
 
 pizzaInput2.addEventListener("input", () => {
   const pizzaSize1 = pizzaInput1.value;
   const pizzaSize2 = pizzaInput2.value;
   calculatePizzaGain(pizzaSize1, pizzaSize2);
+  updatePizzaDisplay(pizza2, pizzaSize2);
 });
 
 // Task 1
@@ -32,6 +34,11 @@ function calculatePizzaGain(d1, d2) {
 
 // Task 2
 // define the function updatePizzaDisplay here
+
+function updatePizzaDisplay(pizzaElement, newSize) {
+  const newDisplaySize = (newSize / 24) * 100;
+  pizzaElement.style.width = `${newDisplaySize}px`;
+}
 
 // Task 3
 // define the function updateOutputColor here
