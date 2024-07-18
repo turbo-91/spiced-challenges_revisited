7cd359e29c6e75f89ecb38092056fbcd33553035
Torben Jost <torbenjost@proton.me>
2024-07-18 12:30:47 +0200

    calculate pizza gain

--- js-functions/pizza-analyzer/js/index.js
+++ js-functions/pizza-analyzer/js/index.js
@@ -9,15 +9,26 @@ const outputSection = document.querySelector('[data-js="output-section"]');
 const output = document.querySelector('[data-js="output"]');
 
 pizzaInput1.addEventListener("input", () => {
-  // write your code here
+  const pizzaSize1 = pizzaInput1.value;
+  const pizzaSize2 = pizzaInput2.value;
+  calculatePizzaGain(pizzaSize1, pizzaSize2);
 });
 
 pizzaInput2.addEventListener("input", () => {
-  // write your code here
+  const pizzaSize1 = pizzaInput1.value;
+  const pizzaSize2 = pizzaInput2.value;
+  calculatePizzaGain(pizzaSize1, pizzaSize2);
 });
 
 // Task 1
 // define the function calculatePizzaGain here
+
+function calculatePizzaGain(d1, d2) {
+  const area1 = (Math.PI * (d1 * d1)) / 4;
+  const area2 = (Math.PI * (d2 * d2)) / 4;
+  const pizzaGainInPercent = ((area2 - area1) / area1) * 100;
+  output.textContent = Math.round(pizzaGainInPercent);
+}
 
 // Task 2
 // define the function updatePizzaDisplay here
