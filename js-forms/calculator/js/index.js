1fdfcfb0c74bc2f14d6d4a399f4dd90eddbe1c99
Torben Jost <torbenjost@proton.me>
2024-07-22 12:25:21 +0200

    solve challenge

--- js-forms/calculator/js/index.js
+++ js-forms/calculator/js/index.js
@@ -23,10 +23,27 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   let result;
+  const formElements = event.target.elements;
+  const numberA = +formElements["numberA"].value;
+  const numberB = +formElements["numberB"].value;
 
-  // --v-- write your code here --v--
-
-  // --^-- write your code here --^--
-
+  if (formElements["addition"].checked) {
+    result = add(numberA, numberB);
+  }
+  if (formElements["subtraction"].checked) {
+    result = subtract(numberA, numberB);
+  }
+  if (formElements["multiplication"].checked) {
+    result = multiply(numberA, numberB);
+  }
+  if (formElements["division"].checked) {
+    if (numberB === 0) {
+      result = "Error: Division by zero";
+    } else {
+      result = divide(numberA, numberB);
+    }
+  } else {
+    result = "Please select an operation";
+  }
   resultOutput.textContent = result;
 });
