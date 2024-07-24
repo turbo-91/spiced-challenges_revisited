46c4cb5d2d4041c488f9ab745b4b8bf4ee56110e
Torben Jost <torbenjost@proton.me>
2024-07-24 13:39:08 +0200

    render stars

--- js-loops/stars/js/index.js
+++ js-loops/stars/js/index.js
@@ -5,6 +5,11 @@ const starContainer = document.querySelector('[data-js="star-container"]');
 function renderStars() {
   // reset the star container before rerendering stars
   starContainer.innerHTML = "";
+  for (let counter = 1; counter <= 5; counter++) {
+    const image = document.createElement("img");
+    image.setAttribute("src", "assets/star-empty.svg");
+    starContainer.append(image);
+  }
 
   //--v-- your code here --v--
 
