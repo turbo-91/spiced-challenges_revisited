5214b25a71380d512aad4eceae4ffb04ddd783ad
Torben Jost <torbenjost@proton.me>
2024-07-24 13:45:43 +0200

    change the number of filled stars C

--- js-loops/stars/js/index.js
+++ js-loops/stars/js/index.js
@@ -2,18 +2,18 @@ console.clear();
 
 const starContainer = document.querySelector('[data-js="star-container"]');
 
-function renderStars() {
+function renderStars(filledStars) {
   // reset the star container before rerendering stars
   starContainer.innerHTML = "";
   for (let counter = 1; counter <= 5; counter++) {
     const image = document.createElement("img");
-    image.setAttribute("src", "assets/star-empty.svg");
+    if (counter <= filledStars) {
+      image.setAttribute("src", "assets/star-filled.svg"); // use filled star image
+    } else {
+      image.setAttribute("src", "assets/star-empty.svg"); // use empty star image
+    }
     starContainer.append(image);
   }
-
-  //--v-- your code here --v--
-
-  //--^-- your code here --^--
 }
 
 renderStars();
