747e95fcba95ab0e0c350250933c3a508b2c6b11
Torben Jost <torbenjost@proton.me>
2024-07-31 08:45:57 +0200

    solve challenge

--- js-fetch/star-wars/js/index.js
+++ js-fetch/star-wars/js/index.js
@@ -41,6 +41,15 @@ fetchDataAndRender();
 
 // --v-- your code below this line --v--
 
-function fetchDataAndRender() {
-  fetch(); // ?
+async function fetchDataAndRender() {
+  const response = await fetch("https://swapi.py4e.com/api/people");
+  const data = await response.json();
+  console.log("data", data);
+  console.log("data.results", data.results);
+
+  const characters = data.results;
+  characters.forEach((character) => {
+    const nextCard = Card(character);
+    renderElement(nextCard);
+  });
 }
